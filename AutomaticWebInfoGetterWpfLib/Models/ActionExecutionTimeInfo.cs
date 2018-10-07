c51bac53e8a2b95077666423570a9a0e39da49f7
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-07 13:21:43 +0400

    Created ActionExecutionFrequency and SettingsInfo model classes

---
+++ AutomaticWebInfoGetterWpfLib/Models/ActionExecutionTimeInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticWebInfoGetterWpfLib.Models
+{
+    class ActionExecutionTimeInfo
+    {
+        DateTime startDate;
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+
+        TimeSpan actionExecutionFrequency;
+        public TimeSpan ActionExecutionFrequency { get => actionExecutionFrequency; set => actionExecutionFrequency = value; }
+
+        DateTime endDate;
+        public DateTime EndDate { get => endDate; set => endDate = value; }
+    
+    }
+}
---
+++ AutomaticWebInfoGetterWpfLib/Models/SettingsInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticWebInfoGetterWpfLib.Models
+{
+    class SettingsInfo
+    {
+        string url;
+        public string URL { get => url; set => url = value; }
+
+        string xpath;
+        public string XPath { get => xpath; set => XPath = value; }
+
+        bool singleNode;
+        public bool SingleNode { get => singleNode; set => singleNode = value; }
+
+        ActionExecutionTimeInfo timeInfo;
+        public ActionExecutionTimeInfo TimeInfo { get => timeInfo; set => timeInfo = value; }
+    }
+}
