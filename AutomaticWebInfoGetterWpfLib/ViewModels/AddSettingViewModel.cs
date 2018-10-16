24862579f4dcca50785ef158f86444516320a858
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-16 21:17:46 +0400

    Added properties and fields in AddSettingViewModel.cs

--- AutomaticWebInfoGetterWpfLib/ViewModels/AddSettingViewModel.cs
+++ AutomaticWebInfoGetterWpfLib/ViewModels/AddSettingViewModel.cs
@@ -9,5 +9,60 @@ namespace AutomaticWebInfoGetterWpfLib.ViewModels
 {
     class AddSettingViewModel: ViewModelBase
     {
+        #region Fields and Properties
+        private string url;
+
+        public string URL
+        {
+            get { return url; }
+            set { Set(ref url, value); }
+        }
+
+        private string xpath;
+
+        public string XPath
+        {
+            get { return xpath; }
+            set { Set(ref xpath, value); }
+        }
+
+        private bool isSingleNode;
+
+        public bool IsSingleNode
+        {
+            get { return isSingleNode; }
+            set { Set(ref isSingleNode, value); }
+        }
+
+
+        private TimeSpan delayBetweenQueries;
+
+        public TimeSpan DelayBetweenQueries
+        {
+            get { return delayBetweenQueries; }
+            set { delayBetweenQueries = value; }
+        }
+
+
+        private DateTime startDate;
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+
+        private DateTime endDate;
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+
+        #endregion
+
     }
 }
