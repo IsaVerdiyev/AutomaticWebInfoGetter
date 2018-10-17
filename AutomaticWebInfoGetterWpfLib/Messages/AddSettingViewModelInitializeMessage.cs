c2ffcc62af1584aa14b018914b63e37b586953e1
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-17 14:56:25 +0400

    Added AddSettingViewModelInitializeMessage and SettingsViewModelAddSettingInfoMessage for reseting vew of AddSettingView and adding new SettingInfo into SettingInfos collection in SettingsInfoViewModel accordingly

---
+++ AutomaticWebInfoGetterWpfLib/Messages/AddSettingViewModelInitializeMessage.cs
@@ -0,0 +1,13 @@
+﻿using AutomaticWebInfoGetterWpfLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticWebInfoGetterWpfLib.Messages
+{
+    class AddSettingViewModelInitializeMessage
+    {
+    }
+}
---
+++ AutomaticWebInfoGetterWpfLib/Messages/SettingsViewModelAddSettingInfoMessage.cs
@@ -0,0 +1,14 @@
+﻿using AutomaticWebInfoGetterWpfLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticWebInfoGetterWpfLib.Messages
+{
+    class SettingsViewModelAddSettingInfoMessage
+    {
+        public SettingsInfo AddedSettingInfo { get; set; }
+    }
+}
