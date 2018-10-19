320ac21d86e259616199a996d6084dc50044fdf7
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-19 23:33:47 +0400

    Added exception which will thrown whenever there is no any appropriate type in DelayMeasuresEnum

---
+++ AutomaticWebInfoGetterWpfLib/Exceptions/NoSuchEnumInTimespanToStringMultiConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticWebInfoGetterWpfLib.Exceptions
+{
+    class NoSuchTypeInDelayMeasuresEnum: Exception
+    {
+        public NoSuchTypeInDelayMeasuresEnum()
+        {
+
+        }
+
+        public NoSuchTypeInDelayMeasuresEnum(string message): base(message)
+        {
+
+        }
+    }
+}
