76f6c38d17d3dbd892578ee7f860ea430c95bdff
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-19 23:30:20 +0400

    Added converters for displaying isSingle bool type in SettingsView and for displaying delay depending of type of delay measure

---
+++ AutomaticWebInfoGetterWpfLib/Converters/DelayBetweenQueriesTimespanToStringMultiConverter.cs
@@ -0,0 +1,51 @@
+﻿using AutomaticWebInfoGetterWpfLib.Exceptions;
+using AutomaticWebInfoGetterWpfLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AutomaticWebInfoGetterWpfLib.Converters
+{
+    class DelayBetweenQueriesTimespanToStringMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((Visibility)values[2] == Visibility.Collapsed)
+            {
+                return 0;
+            }
+            TimeSpan delayBetweenQueries = (TimeSpan)values[0];
+            string selectedDelayMeasure = (string)values[1];
+            if(selectedDelayMeasure == DelayMeasuresEnum.Seconds.ToString())
+            {
+                return delayBetweenQueries.TotalSeconds.ToString();
+            }
+            else if (selectedDelayMeasure == DelayMeasuresEnum.Minutes.ToString())
+            {
+                return delayBetweenQueries.TotalMinutes.ToString();
+            }
+            else if(selectedDelayMeasure == DelayMeasuresEnum.Hours.ToString())
+            {
+                return delayBetweenQueries.TotalHours.ToString();
+            }
+            else if (selectedDelayMeasure == DelayMeasuresEnum.Days.ToString())
+            {
+                return delayBetweenQueries.TotalDays.ToString();
+            }
+            else
+            {
+                throw new NoSuchTypeInDelayMeasuresEnum();
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutomaticWebInfoGetterWpfLib/Converters/IsSingleBoolToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AutomaticWebInfoGetterWpfLib.Converters
+{
+    class IsSingleBoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSingleNode = (Boolean)value;
+            if (isSingleNode)
+            {
+                return "One node";
+            }
+            else
+            {
+                return "Collection of nodes";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
