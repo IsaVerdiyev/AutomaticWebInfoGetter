e404551677eb37b4dd2cc07fec66e907a2ab0ef8
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-22 16:33:20 +0400

    Created new service 'TimerInitializer'. This service will be used to initialize timer of SettingsInfo models.

---
+++ AutomaticWebInfoGetterWpfLib/Services/TimerInitializer/ITimerInitializer.cs
@@ -0,0 +1,16 @@
+﻿using AutomaticWebInfoGetterWpfLib.Models;
+using AutomaticWebInfoGetterWpfLib.Services.WebInfoGetter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomaticWebInfoGetterWpfLib.Services.TimerInitializer
+{
+    interface ITimerInitializer
+    {
+        void InitializeTimer(SettingsInfo settingsInfo, IWebInfoGetter webInfogetter);
+    }
+}
---
+++ AutomaticWebInfoGetterWpfLib/Services/TimerInitializer/TimerInitializer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutomaticWebInfoGetterWpfLib.Models;
+using AutomaticWebInfoGetterWpfLib.Services.WebInfoGetter;
+
+namespace AutomaticWebInfoGetterWpfLib.Services.TimerInitializer
+{
+    class TimerInitializer : ITimerInitializer
+    {
+
+        public void InitializeTimer(SettingsInfo settingsInfo, IWebInfoGetter webInfogetter)
+        {
+            DateTime whenToStart = settingsInfo.TimeInfo.StartDate;
+            while (whenToStart < DateTime.Now && whenToStart < settingsInfo.TimeInfo.EndDate)
+            {
+                whenToStart = whenToStart.Add(settingsInfo.TimeInfo.DelayBetweenQueries);
+            }
+
+            if (whenToStart > settingsInfo.TimeInfo.EndDate)
+            {
+                return;
+            }
+
+            settingsInfo.Timer = new Timer(o1 => Task.Run(() => 
+            {
+                if (settingsInfo.SingleNode)
+                {
+                    File.AppendAllText("TimerResult.txt", webInfogetter.GetStringOfNodeByXPathFromUrl(settingsInfo.URL, settingsInfo.XPath));
+                }
+                else
+                { 
+                    File.AppendAllLines("TimerResult.txt", webInfogetter.GetStringsOfNodesByXPathFromUrl(settingsInfo.URL, settingsInfo.XPath));
+                }
+                whenToStart = whenToStart.Add(settingsInfo.TimeInfo.DelayBetweenQueries);
+                if(whenToStart < settingsInfo.TimeInfo.EndDate)
+                {
+                    TimeSpan timeDifference = whenToStart - DateTime.Now;
+                    settingsInfo.Timer.Change((int)timeDifference.TotalMilliseconds, Timeout.Infinite);
+                }
+                
+
+            }), null, Timeout.Infinite, Timeout.Infinite);
+            settingsInfo.Timer.Change((whenToStart - settingsInfo.TimeInfo.StartDate).Milliseconds, Timeout.Infinite);
+
+
+        }
+    }
+}
