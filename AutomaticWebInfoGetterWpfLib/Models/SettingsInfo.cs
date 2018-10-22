4466a0e095514310d226c312262ab503aea0f09c
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-22 16:31:04 +0400

    Added new full property Timer in SettingsInfo model. Now getting info from web and saving information will be executed by that timer

--- AutomaticWebInfoGetterWpfLib/Models/SettingsInfo.cs
+++ AutomaticWebInfoGetterWpfLib/Models/SettingsInfo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutomaticWebInfoGetterWpfLib.Models
@@ -19,5 +20,14 @@ namespace AutomaticWebInfoGetterWpfLib.Models
 
         ActionExecutionTimeInfo timeInfo;
         public ActionExecutionTimeInfo TimeInfo { get => timeInfo; set => timeInfo = value; }
+
+        private Timer timer;
+
+        public Timer Timer
+        {
+            get { return timer; }
+            set { timer = value; }
+        }
+
     }
 }
