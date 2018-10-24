08c9272218ff14874764bb6ad49a918854daf7d5
Вердиев Иса Махир <Verd_ym80@ITSTEP.LOCAL>
2018-10-24 16:31:01 +0400

    Added new converter for converting bool, representing orientation of writing in SettingsInfo model, into string. Also included it in SettingsViewDicitionary as plan to use it SettingsView.

---
+++ AutomaticWebInfoGetterWpfLib/Converters/OrientationOfWritingBoolToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+namespace AutomaticWebInfoGetterWpfLib.Converters
+{
+    class OrientationOfWritingBoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool HorizontalOrientation = (bool)value;
+            if (HorizontalOrientation)
+            {
+                return "Horizontal writing of info";
+            }
+            else
+            {
+                return "Vertical writing of info";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
