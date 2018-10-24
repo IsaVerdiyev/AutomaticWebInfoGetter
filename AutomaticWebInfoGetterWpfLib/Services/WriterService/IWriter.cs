3c99e11c96fb2836119898952cac46097b4df687
Вердиев Иса Махир <Verd_ym80@ITSTEP.LOCAL>
2018-10-24 16:26:17 +0400

    Created new service interface for writing in file and implemented it in WriterToExcelfile.

---
+++ AutomaticWebInfoGetterWpfLib/Services/WriterService/IWriter.cs
@@ -0,0 +1,14 @@
+﻿using AutomaticWebInfoGetterWpfLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AutomaticWebInfoGetterWpfLib.Services.WriterService
+{
+    interface IWriter
+    {
+        void WriteToExcel(List<String> info, SettingsInfo settingsInfo, DownloadedPartOfPageSettingInfo downloadedPart);
+        void WriteToExcel(string info, SettingsInfo settingsInfo, DownloadedPartOfPageSettingInfo downloadedPart);
+    }
+}
---
+++ AutomaticWebInfoGetterWpfLib/Services/WriterService/WriterToExcel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomaticWebInfoGetterWpfLib.Models;
+using OfficeOpenXml;
+namespace AutomaticWebInfoGetterWpfLib.Services.WriterService
+{
+    class GetWorkingSheet : IWriter
+    {
+        public void WriteToExcel(List<string> infos, SettingsInfo settingsInfo, DownloadedPartOfPageSettingInfo downloadedPart)
+        {
+            var file = new FileInfo(settingsInfo.NameOfFileToWriteInfo);
+            using (var excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet workSheet;
+                InitializeWorkSheet(excelPackage, out workSheet);
+                InstallCurrentWritingPositionIfIsNull(downloadedPart, settingsInfo, workSheet);
+                for (int i = 0; i < infos.Count(); i++)
+                {
+                    workSheet.Cells[downloadedPart.CurrentWritingPosition.Row, downloadedPart.CurrentWritingPosition.Column].Value = infos[i];
+                    if (settingsInfo.HorizontalOrientationOfWritingInfo)
+                    {
+                        downloadedPart.CurrentWritingPosition.Column += settingsInfo.BetweenLineDistance;
+                    }
+                    else
+                    {
+                        downloadedPart.CurrentWritingPosition.Row += settingsInfo.BetweenLineDistance;
+                    }
+                }
+            }
+        }
+        public void WriteToExcel(string info, SettingsInfo settingsInfo, DownloadedPartOfPageSettingInfo downloadedPart)
+        {
+            var file = new FileInfo(settingsInfo.NameOfFileToWriteInfo);
+            using (var excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet workSheet;
+                InitializeWorkSheet(excelPackage, out workSheet);
+                InstallCurrentWritingPositionIfIsNull(downloadedPart, settingsInfo, workSheet);
+                workSheet.Cells[downloadedPart.CurrentWritingPosition.Row, downloadedPart.CurrentWritingPosition.Column].Value = info;
+                if (settingsInfo.HorizontalOrientationOfWritingInfo)
+                {
+                    downloadedPart.CurrentWritingPosition.Column += settingsInfo.BetweenLineDistance;
+                }
+                else
+                {
+                    downloadedPart.CurrentWritingPosition.Row += settingsInfo.BetweenLineDistance;
+                }
+            }
+        }
+        private void InstallCurrentWritingPositionIfIsNull(DownloadedPartOfPageSettingInfo downloadedPartInfo, SettingsInfo settingsInfo, ExcelWorksheet workSheet)
+        {
+            if (downloadedPartInfo.CurrentWritingPosition == null)
+            {
+                if (downloadedPartInfo.Header != null)
+                {
+                    workSheet.Cells[downloadedPartInfo.StartPositionOfWriting.Row, downloadedPartInfo.StartPositionOfWriting.Column].Value = downloadedPartInfo.Header;
+                    if (settingsInfo.HorizontalOrientationOfWritingInfo)
+                    {
+                        downloadedPartInfo.CurrentWritingPosition = new Position
+                        {
+                            Row = downloadedPartInfo.StartPositionOfWriting.Row,
+                            Column = downloadedPartInfo.StartPositionOfWriting.Column + settingsInfo.BetweenLineDistance
+                        };
+                    }
+                    else
+                    {
+                        downloadedPartInfo.CurrentWritingPosition = new Position
+                        {
+                            Row = downloadedPartInfo.StartPositionOfWriting.Row + settingsInfo.BetweenLineDistance,
+                            Column = downloadedPartInfo.StartPositionOfWriting.Column
+                        };
+                    }
+                    workSheet.Cells[downloadedPartInfo.StartPositionOfWriting.Row, downloadedPartInfo.StartPositionOfWriting.Column].Style.Font.Color.SetColor(Color.Blue);
+                }
+                else
+                {
+                    downloadedPartInfo.CurrentWritingPosition = (Position)downloadedPartInfo.StartPositionOfWriting.Clone();
+                }
+            }
+        }
+        private void InitializeWorkSheet(ExcelPackage excelPackage, out ExcelWorksheet worksheet)
+        {
+            if (excelPackage.Workbook.Worksheets.Count == 0)
+            {
+                worksheet = excelPackage.Workbook.Worksheets.Add("Worksheet 1");
+            }
+            else
+            {
+                worksheet = excelPackage.Workbook.Worksheets.First();
+            }
+        }
+    }
+}
