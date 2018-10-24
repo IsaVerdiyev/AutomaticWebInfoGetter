272199a3f3e75a5aba933d8b2ccbc50efb185844
Вердиев Иса Махир <Verd_ym80@ITSTEP.LOCAL>
2018-10-24 16:18:10 +0400

    Created new model Position which will be used for saving position of writing to file

---
+++ AutomaticWebInfoGetterWpfLib/Models/Position.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AutomaticWebInfoGetterWpfLib.Models
+{
+    class Position : ICloneable
+    {
+        private int row;
+        public int Row
+        {
+            get { return row; }
+            set { row = value; }
+        }
+        private int column;
+        public int Column
+        {
+            get { return column; }
+            set { column = value; }
+        }
+        public object Clone()
+        {
+            return new Position
+            {
+                Row = Row,
+                Column = Column
+            };
+        }
+    }
+}
