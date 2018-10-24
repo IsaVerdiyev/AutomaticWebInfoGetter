ab1bc3b0d85c1f551dd9f575c6a87f651fa0c77c
Вердиев Иса Махир <Verd_ym80@ITSTEP.LOCAL>
2018-10-24 16:21:49 +0400

    Added new model DownloadedPartOfPageSettingInfo. Now in SettingsInfo instead of XPath will be stored collection of this model. Inside this model will be stored positions of type Position for writing in file purposes, XPath and header for writing header in file. So from one page it can be downloaded info from several XPathes and that info will be stored in this collection.

---
+++ AutomaticWebInfoGetterWpfLib/Models/DownloadedPartOfPageSettingInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AutomaticWebInfoGetterWpfLib.Models
+{
+    class DownloadedPartOfPageSettingInfo
+    {
+        private string xpath;
+        public string XPath
+        {
+            get { return xpath; }
+            set { xpath = value; }
+        }
+        private string header;
+        public string Header
+        {
+            get { return header; }
+            set { header = value; }
+        }
+        private Position startPositionOfWriting;
+        public Position StartPositionOfWriting
+        {
+            get { return startPositionOfWriting; }
+            set { startPositionOfWriting = value; }
+        }
+        private Position currentWritingPosition;
+        public Position CurrentWritingPosition
+        {
+            get { return currentWritingPosition; }
+            set { currentWritingPosition = value; }
+        }
+    }
+}
