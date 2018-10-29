fdb380d07b24701cae88f569e44f26cd01f2d401
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-10-29 12:06:46 +0400

    Adjusted WebInfoGetterBasedOnSeleniumAndChrome service: now if some alerts occur while trying to get info from page - alert will be ignored

--- AutomaticWebInfoGetterWpfLib/Services/WebInfoGetter/WebInfoGetterBasedOnSeleniumAndChrome.cs
+++ AutomaticWebInfoGetterWpfLib/Services/WebInfoGetter/WebInfoGetterBasedOnSeleniumAndChrome.cs
@@ -33,12 +33,18 @@ namespace AutomaticWebInfoGetterWpfLib.Services.WebInfoGetter
 
             lock (lockObject)
             {
-
-                webDriver.Url = url;
-
                 WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(20));
+                try
+                {
+                    webDriver.Url = url;
 
-                element = wait.Until((webdriver) => webDriver.FindElement(By.XPath(xpath)));
+
+                    element = wait.Until((webdriver) => webDriver.FindElement(By.XPath(xpath)));
+                }catch(UnhandledAlertException e)
+                {
+                    DismissAlert();
+                    element = wait.Until((webdriver) => webDriver.FindElement(By.XPath(xpath)));
+                }
             }
 
             return element.Text;
@@ -50,14 +56,30 @@ namespace AutomaticWebInfoGetterWpfLib.Services.WebInfoGetter
              
             lock (lockObject)
             {
-                webDriver.Url = url;
-
                 WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(20));
+                try
+                {
+                    webDriver.Url = url;
 
-                elements = wait.Until((webdriver) => webDriver.FindElements(By.XPath(xpath)));
+
+                    elements = wait.Until((webdriver) => webDriver.FindElements(By.XPath(xpath)));
+                }catch(UnhandledAlertException e)
+                {
+
+                    DismissAlert();
+                    elements = wait.Until((webdriver) => webDriver.FindElements(By.XPath(xpath)));
+
+                }
 
             }
             return elements.Select(e => e.Text).ToList();
+        }
+
+
+        void DismissAlert()
+        {
+            var alert = webDriver.SwitchTo().Alert();
+            alert.Dismiss();
         }
 
         ~WebInfoGetterBasedOnSeleniumAndChrome (){
