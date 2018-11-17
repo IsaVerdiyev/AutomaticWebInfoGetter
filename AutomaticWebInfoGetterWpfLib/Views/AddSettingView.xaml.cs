ffdcd44ce3d4fc95c2039414cb498cf33d9f5f4e
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-11-17 14:39:38 +0400

    Adjusted animation of opening but yet have some bugs with it

--- AutomaticWebInfoGetterWpfLib/Views/AddSettingView.xaml.cs
+++ AutomaticWebInfoGetterWpfLib/Views/AddSettingView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutomaticWebInfoGetterWpfLib.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,11 +20,75 @@ namespace AutomaticWebInfoGetterWpfLib.Views
     /// <summary>
     /// Interaction logic for AddSettingView.xaml
     /// </summary>
+    ///
     public partial class AddSettingView : UserControl
     {
+
+        public Storyboard openingStoryboard;
+
         public AddSettingView()
         {
             InitializeComponent();
+
+            //openingStoryboard = new Storyboard();
+            //var animation = new ThicknessAnimation();
+            //animation.BeginTime = new TimeSpan(0);
+            //Storyboard.SetTargetName(animation, XPathAddingGrid.Name);
+            //Storyboard.SetTargetProperty(animation, new PropertyPath(Grid.MarginProperty));
+            //animation.To = new Thickness(0, 0, 0, 0);
+            //animation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+            //openingStoryboard.Children.Add(animation);
+
+
+            
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void XPathAddingGrid_LayoutUpdated(object sender, EventArgs e)
+        {
+            var animation = ((Resources["OpeningAddXPathGridAnimation"] as Storyboard).Children.First()) as ThicknessAnimationUsingKeyFrames;
+            Storyboard.SetTargetName(animation, XPathAddingGrid.Name);
+            animation.KeyFrames[0].Value = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
+
+            animation.KeyFrames[1].Value = new Thickness(0, 0, 0, 0);
+            animation.Completed += (s, evargs) => XPathAddingGrid.Margin = new Thickness(0, 0, 0, 0);
+            //animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+
+
+            animation = ((Resources["ClosingAddXPathGridAnimation"] as Storyboard).Children.First()) as ThicknessAnimationUsingKeyFrames;
+            Storyboard.SetTargetName(animation, XPathAddingGrid.Name);
+            animation.KeyFrames[0].Value = new Thickness(0, 0, 0, 0);
+
+            animation.KeyFrames[1].Value = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
+            animation.Completed += (s, evargs) =>
+            {
+                Binding binding = new Binding();
+                binding.Source = XPathAddingGrid.ActualHeight;
+                XPathAddingGrid.SetBinding(MarginProperty, binding);
+            };
+
+            animation.Completed += (s, evargs) =>
+            {
+                Binding binding = new Binding("ActualHeightProperty");
+                binding.Source = XPathAddingGrid;
+                binding.Mode = BindingMode.OneWay;
+                binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+                binding.Converter = Resources["MarginConverter"] as MarginConverter;
+                binding.ConverterParameter = MarginConverterSideOptions.Top;
+                XPathAddingGrid.SetBinding(MarginProperty, binding);
+            };
+            //animation.Completed += (s, evargs) => XPathAddingGrid.Margin = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
+            //animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+        }
+
+        private void XPathAddingGrid_LayoutUpdated(object sender, SizeChangedEventArgs e)
+        {
+
         }
     }
 }
