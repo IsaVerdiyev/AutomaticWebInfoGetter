93225cd47e22c1286ced55b3a5c8b598de15c10b
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-11-20 13:31:54 +0400

    Decided to change AddSettingView totally and its styles. Now all controls will have fixed size

---
+++ AutomaticWebInfoGetterWpfLib/Converters/HeightToMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AutomaticWebInfoGetterWpfLib.Converters
+{
+    class HeightToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double height = (Double)value;
+
+            return new Thickness(0, -height, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AutomaticWebInfoGetterWpfLib/Views/AddSettingView.xaml.cs
+++ AutomaticWebInfoGetterWpfLib/Views/AddSettingView.xaml.cs
@@ -29,42 +29,42 @@ namespace AutomaticWebInfoGetterWpfLib.Views
         public AddSettingView()
         {
             InitializeComponent();
-            var storyboard = Resources["OpeningAddXPathGridStoryboard"] as Storyboard;
-            var openingMarginAnimation = storyboard.Children.First(a => a.Name == "OpeningMarginAnimation") as ThicknessAnimationUsingKeyFrames;
-            Storyboard.SetTargetName(openingMarginAnimation, XPathAddingGrid.Name);
-            openingMarginAnimation.Completed += (s, evargs) => XPathAddingGrid.Margin = new Thickness(0, 0, 0, 0);
+            //var storyboard = Resources["OpeningAddXPathGridStoryboard"] as Storyboard;
+            //var openingMarginAnimation = storyboard.Children.First(a => a.Name == "OpeningMarginAnimation") as ThicknessAnimationUsingKeyFrames;
+            //Storyboard.SetTargetName(openingMarginAnimation, XPathAddingGrid.Name);
+            //openingMarginAnimation.Completed += (s, evargs) => XPathAddingGrid.Margin = new Thickness(0, 0, 0, 0);
 
-            //var openingHeightAnimation = storyboard.Children.First(a => a.Name == "OpeningHeightAnimation") as DoubleAnimation;
-            //Storyboard.SetTargetName(openingMarginAnimation, InfoAddingGrid.Name);
+            ////var openingHeightAnimation = storyboard.Children.First(a => a.Name == "OpeningHeightAnimation") as DoubleAnimation;
+            ////Storyboard.SetTargetName(openingMarginAnimation, InfoAddingGrid.Name);
 
 
-            storyboard = Resources["ClosingAddXPathGridStoryboard"] as Storyboard;
-            openingMarginAnimation = storyboard.Children.First(a => a.Name == "ClosingMarginAnimation") as ThicknessAnimationUsingKeyFrames;
-            Storyboard.SetTargetName(openingMarginAnimation, XPathAddingGrid.Name);
-            openingMarginAnimation.Completed += (s, evargs) =>
-            {
+            //storyboard = Resources["ClosingAddXPathGridStoryboard"] as Storyboard;
+            //openingMarginAnimation = storyboard.Children.First(a => a.Name == "ClosingMarginAnimation") as ThicknessAnimationUsingKeyFrames;
+            //Storyboard.SetTargetName(openingMarginAnimation, XPathAddingGrid.Name);
+            //openingMarginAnimation.Completed += (s, evargs) =>
+            //{
 
-                Binding binding = new Binding("ActualHeight");
-                binding.Source = XPathAddingGrid;
-                binding.Mode = BindingMode.OneWay;
-                binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
-                binding.Converter = Resources["MarginConverter"] as MarginConverter;
-                binding.ConverterParameter = MarginConverterSideOptions.Top;
-                XPathAddingGrid.SetBinding(Grid.MarginProperty, binding);
-            };
+            //    Binding binding = new Binding("ActualHeight");
+            //    binding.Source = XPathAddingGrid;
+            //    binding.Mode = BindingMode.OneWay;
+            //    binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            //    binding.Converter = Resources["MarginConverter"] as MarginConverter;
+            //    binding.ConverterParameter = MarginConverterSideOptions.Top;
+            //    XPathAddingGrid.SetBinding(Grid.MarginProperty, binding);
+            //};
         }
 
-        private void RefreshAnimationKeyFrames(object sender, EventArgs e)
-        {
-            var storyboard = Resources["OpeningAddXPathGridStoryboard"] as Storyboard;
-            var animation = storyboard.Children.First(a => a.Name == "OpeningMarginAnimation") as ThicknessAnimationUsingKeyFrames;
-            animation.KeyFrames[0].Value = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
-            animation.KeyFrames[1].Value = new Thickness(0, 0, 0, 0);
+        //private void RefreshAnimationKeyFrames(object sender, EventArgs e)
+        //{
+        //    var storyboard = Resources["OpeningAddXPathGridStoryboard"] as Storyboard;
+        //    var animation = storyboard.Children.First(a => a.Name == "OpeningMarginAnimation") as ThicknessAnimationUsingKeyFrames;
+        //    animation.KeyFrames[0].Value = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
+        //    animation.KeyFrames[1].Value = new Thickness(0, 0, 0, 0);
             
-            storyboard = Resources["ClosingAddXPathGridStoryboard"] as Storyboard;
-            animation = storyboard.Children.First(a => a.Name == "ClosingMarginAnimation") as ThicknessAnimationUsingKeyFrames;
-            animation.KeyFrames[0].Value = new Thickness(0, 0, 0, 0);
-            animation.KeyFrames[1].Value = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
-        }
+        //    storyboard = Resources["ClosingAddXPathGridStoryboard"] as Storyboard;
+        //    animation = storyboard.Children.First(a => a.Name == "ClosingMarginAnimation") as ThicknessAnimationUsingKeyFrames;
+        //    animation.KeyFrames[0].Value = new Thickness(0, 0, 0, 0);
+        //    animation.KeyFrames[1].Value = new Thickness(0, -XPathAddingGrid.ActualHeight, 0, 0);
+        //}
     }
 }
